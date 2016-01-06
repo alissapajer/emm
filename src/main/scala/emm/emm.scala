393e613cd8a49e529d6d854197090a5389fd2862
Alissa Pajer <alissa.pajer@gmail.com>
2016-01-06 11:12:59 -0800

    Reenabled Lifter

--- src/main/scala/emm/emm.scala
+++ src/main/scala/emm/emm.scala
@@ -780,16 +780,18 @@ object Effects {
     }
   }
 
-  /*@implicitNotFound("could not lift ${E} into stack ${C}; either ${C} does not contain a constructor of ${E}, or there is no Functor for a constructor of ${E}")
+  @implicitNotFound("could not lift ${E} into stack ${C}; either ${C} does not contain a constructor of ${E}, or there is no Functor for a constructor of ${E}")
   trait Lifter[E, C <: Effects] {
     type Out
+    type CC[A] = C#Point[A]
 
-    def apply(e: E): C#Point[Out]
+    def apply(e: E): CC[Out]
   }
 
   trait LifterLowPriorityImplicits {
     import cats.state.State
 
+    /*
     implicit def headState[S, A]: Lifter.Aux[State[S, A], State[S, ?] |: Base, A] = new Lifter[State[S, A], State[S, ?] |: Base] {
       type Out = A
 
@@ -807,28 +809,17 @@ object Effects {
 
       def apply(e: E) = State.pure(L(e))
     }
+    */
   }
 
   object Lifter extends LifterLowPriorityImplicits {
     type Aux[E, C <: Effects, Out0] = Lifter[E, C] { type Out = Out0 }
 
-    implicit def head1[F[_], A]: Lifter.Aux[F[A], F |: Base, A] = new Lifter[F[A], F |: Base] {
-      type Out = A
+    //implicit def base[A]: Lifter[A, Base] = new Lifter[A, Base] {
+    //  type Out = A
 
-      def apply(fa: F[A]) = fa
-    }
-
-    implicit def head2[F[_, _], F2[_, _], Z, A](implicit ev: Permute2[F, F2]): Lifter.Aux[F2[Z, A], F2[Z, ?] |: Base, A] = new Lifter[F2[Z, A], F2[Z, ?] |: Base] {
-      type Out = A
-
-      def apply(fa: F2[Z, A]) = fa
-    }
-
-    implicit def head3[F[_, _, _], F2[_, _, _], Y, Z, A](implicit ev: Permute3[F, F2]): Lifter.Aux[F2[Y, Z, A], F2[Y, Z, ?] |: Base, A] = new Lifter[F2[Y, Z, A], F2[Y, Z, ?] |: Base] {
-      type Out = A
-
-      def apply(fa: F2[Y, Z, A]) = fa
-    }
+    //  def apply(fa: A) = fa
+    //}
 
     implicit def headH1[F[_[_], _], G[_], A]: Lifter.Aux[F[G, A], F[G, ?] |: Base, A] = new Lifter[F[G, A], F[G, ?] |: Base] {
       type Out = A
@@ -848,78 +839,78 @@ object Effects {
       def apply(fa: F2[G, Y, Z, A]) = fa
     }
 
-    implicit def mid1[F[_], A, C <: Effects](implicit C: Mapper[C], F: Functor[F]): Lifter.Aux[F[A], F |: C, A] = new Lifter[F[A], F |: C] {
+    implicit def mid1[F[_], A, C <: Effects](implicit C: Mapper[C], F: Functor[F], NN: NonNested[C]): Lifter.Aux[F[A], F |: C, A] = new Lifter[F[A], F |: C] {
       type Out = A
 
-      def apply(fa: F[A]) = F.map(fa) { a => C.point(a) }
+      def apply(fa: F[A]) = NN.pack(F.map(fa) { a => C.point(a) })
     }
 
-    implicit def mid2[F[_, _], F2[_, _], Z, A, C <: Effects](implicit ev: Permute2[F, F2], C: Mapper[C], F: Functor[F2[Z, ?]]): Lifter.Aux[F2[Z, A], F2[Z, ?] |: C, A] = new Lifter[F2[Z, A], F2[Z, ?] |: C] {
+    implicit def mid2[F[_, _], F2[_, _], Z, A, C <: Effects](implicit ev: Permute2[F, F2], C: Mapper[C], F: Functor[F2[Z, ?]], NN: NonNested[C]): Lifter.Aux[F2[Z, A], F2[Z, ?] |: C, A] = new Lifter[F2[Z, A], F2[Z, ?] |: C] {
       type Out = A
 
-      def apply(fa: F2[Z, A]) = F.map(fa) { a => C.point(a) }
+      def apply(fa: F2[Z, A]) = NN.pack[F2[Z, ?], A](F.map(fa) { a => C.point(a) })
     }
 
-    implicit def mid3[F[_, _, _], F2[_, _, _], Y, Z, A, C <: Effects](implicit ev: Permute3[F, F2], C: Mapper[C], F: Functor[F2[Y, Z, ?]]): Lifter.Aux[F2[Y, Z, A], F2[Y, Z, ?] |: C, A] = new Lifter[F2[Y, Z, A], F2[Y, Z, ?] |: C] {
+    implicit def mid3[F[_, _, _], F2[_, _, _], Y, Z, A, C <: Effects](implicit ev: Permute3[F, F2], C: Mapper[C], F: Functor[F2[Y, Z, ?]], NN: NonNested[C]): Lifter.Aux[F2[Y, Z, A], F2[Y, Z, ?] |: C, A] = new Lifter[F2[Y, Z, A], F2[Y, Z, ?] |: C] {
       type Out = A
 
-      def apply(fa: F2[Y, Z, A]) = F.map(fa) { a => C.point(a) }
+      def apply(fa: F2[Y, Z, A]) = NN.pack[F2[Y, Z, ?], A](F.map(fa) { a => C.point(a) })
     }
 
-    implicit def midH1[F[_[_], _], G[_], A, C <: Effects](implicit C: Mapper[C], F: Functor[F[G, ?]]): Lifter.Aux[F[G, A], F[G, ?] |: C, A] = new Lifter[F[G, A], F[G, ?] |: C] {
+    implicit def midH1[F[_[_], _], G[_], A, C <: Effects](implicit C: Mapper[C], F: Functor[F[G, ?]], NN: NonNested[C]): Lifter.Aux[F[G, A], F[G, ?] |: C, A] = new Lifter[F[G, A], F[G, ?] |: C] {
       type Out = A
 
-      def apply(fa: F[G, A]) = F.map(fa) { a => C.point(a) }
+      def apply(fa: F[G, A]) = NN.pack[F[G, ?], A](F.map((fa)) { a => C.point(a) })
     }
 
-    implicit def midH2[F[_[_], _, _], F2[_[_], _, _], G[_], Z, A, C <: Effects](implicit ev: PermuteH2[F, F2], C: Mapper[C], F: Functor[F2[G, Z, ?]]): Lifter.Aux[F2[G, Z, A], F2[G, Z, ?] |: C, A] = new Lifter[F2[G, Z, A], F2[G, Z, ?] |: C] {
+    implicit def midH2[F[_[_], _, _], F2[_[_], _, _], G[_], Z, A, C <: Effects](implicit ev: PermuteH2[F, F2], C: Mapper[C], F: Functor[F2[G, Z, ?]], NN: NonNested[C]): Lifter.Aux[F2[G, Z, A], F2[G, Z, ?] |: C, A] = new Lifter[F2[G, Z, A], F2[G, Z, ?] |: C] {
       type Out = A
 
-      def apply(fa: F2[G, Z, A]) = F.map(fa) { a => C.point(a) }
+      def apply(fa: F2[G, Z, A]) = NN.pack[F2[G, Z, ?], A](F.map(fa) { a => C.point(a) })
     }
 
-    implicit def midH3[F[_[_], _, _, _], F2[_[_], _, _, _], G[_], Y, Z, A, C <: Effects](implicit ev: PermuteH3[F, F2], C: Mapper[C], F: Functor[F2[G, Y, Z, ?]]): Lifter.Aux[F2[G, Y, Z, A], F2[G, Y, Z, ?] |: C, A] = new Lifter[F2[G, Y, Z, A], F2[G, Y, Z, ?] |: C] {
+    implicit def midH3[F[_[_], _, _, _], F2[_[_], _, _, _], G[_], Y, Z, A, C <: Effects](implicit ev: PermuteH3[F, F2], C: Mapper[C], F: Functor[F2[G, Y, Z, ?]], NN: NonNested[C]): Lifter.Aux[F2[G, Y, Z, A], F2[G, Y, Z, ?] |: C, A] = new Lifter[F2[G, Y, Z, A], F2[G, Y, Z, ?] |: C] {
       type Out = A
 
-      def apply(fa: F2[G, Y, Z, A]) = F.map(fa) { a => C.point(a) }
+      def apply(fa: F2[G, Y, Z, A]) = NN.pack[F2[G, Y, Z, ?], A](F.map(fa) { a => C.point(a) })
     }
 
-    implicit def corecurse1[F[_], E, C <: Effects](implicit L: Lifter[E, C], F: Applicative[F]): Lifter.Aux[E, F |: C, L.Out] = new Lifter[E, F |: C] {
+    implicit def corecurse1[F[_], E, C <: Effects](implicit L: Lifter[E, C], F: Applicative[F], NN: NonNested[C]): Lifter.Aux[E, F |: C, L.Out] = new Lifter[E, F |: C] {
       type Out = L.Out
 
-      def apply(e: E) = F.pure(L(e))
+      def apply(e: E) = NN.pack(F.pure(L(e)))
     }
 
-    implicit def corecurse2[F[_, _], F2[_, _], Z, E, C <: Effects](implicit ev: Permute2[F, F2], L: Lifter[E, C], F: Applicative[F2[Z, ?]]): Lifter.Aux[E, F2[Z, ?] |: C, L.Out] = new Lifter[E, F2[Z, ?] |: C] {
+    implicit def corecurse2[F[_, _], F2[_, _], Z, E, C <: Effects](implicit ev: Permute2[F, F2], L: Lifter[E, C], F: Applicative[F2[Z, ?]], NN: NonNested[C]): Lifter.Aux[E, F2[Z, ?] |: C, L.Out] = new Lifter[E, F2[Z, ?] |: C] {
       type Out = L.Out
 
-      def apply(e: E) = F.pure(L(e))
+      def apply(e: E) = NN.pack[F2[Z, ?], Out](F.pure(L(e)))
     }
 
-    implicit def corecurse3[F[_, _, _], F2[_, _, _], Y, Z, E, C <: Effects](implicit ev: Permute3[F, F2], L: Lifter[E, C], F: Applicative[F2[Y, Z, ?]]): Lifter.Aux[E, F2[Y, Z, ?] |: C, L.Out] = new Lifter[E, F2[Y, Z, ?] |: C] {
+    implicit def corecurse3[F[_, _, _], F2[_, _, _], Y, Z, E, C <: Effects](implicit ev: Permute3[F, F2], L: Lifter[E, C], F: Applicative[F2[Y, Z, ?]], NN: NonNested[C]): Lifter.Aux[E, F2[Y, Z, ?] |: C, L.Out] = new Lifter[E, F2[Y, Z, ?] |: C] {
       type Out = L.Out
 
-      def apply(e: E) = F.pure(L(e))
+      def apply(e: E) = NN.pack[F2[Y, Z, ?], Out](F.pure(L(e)))
     }
 
-    implicit def corecurseH1[F[_[_], _], G[_], E, C <: Effects](implicit L: Lifter[E, C], F: Applicative[F[G, ?]]): Lifter.Aux[E, F[G, ?] |: C, L.Out] = new Lifter[E, F[G, ?] |: C] {
+    implicit def corecurseH1[F[_[_], _], G[_], E, C <: Effects](implicit L: Lifter[E, C], F: Applicative[F[G, ?]], NN: NonNested[C]): Lifter.Aux[E, F[G, ?] |: C, L.Out] = new Lifter[E, F[G, ?] |: C] {
       type Out = L.Out
 
-      def apply(e: E) = F.pure(L(e))
+      def apply(e: E) = NN.pack[F[G, ?], Out](F.pure(L(e)))
     }
 
-    implicit def corecurseH2[F[_[_], _, _], F2[_[_], _, _], G[_], Z, E, C <: Effects](implicit ev: PermuteH2[F, F2], L: Lifter[E, C], F: Applicative[F2[G, Z, ?]]): Lifter.Aux[E, F2[G, Z, ?] |: C, L.Out] = new Lifter[E, F2[G, Z, ?] |: C] {
+    implicit def corecurseH2[F[_[_], _, _], F2[_[_], _, _], G[_], Z, E, C <: Effects](implicit ev: PermuteH2[F, F2], L: Lifter[E, C], F: Applicative[F2[G, Z, ?]], NN: NonNested[C]): Lifter.Aux[E, F2[G, Z, ?] |: C, L.Out] = new Lifter[E, F2[G, Z, ?] |: C] {
       type Out = L.Out
 
-      def apply(e: E) = F.pure(L(e))
+      def apply(e: E) = NN.pack[F2[G, Z, ?], Out](F.pure(L(e)))
     }
 
-    implicit def corecurseH3[F[_[_], _, _, _], F2[_[_], _, _, _], G[_], Y, Z, E, C <: Effects](implicit ev: PermuteH3[F, F2], L: Lifter[E, C], F: Applicative[F2[G, Y, Z, ?]]): Lifter.Aux[E, F2[G, Y, Z, ?] |: C, L.Out] = new Lifter[E, F2[G, Y, Z, ?] |: C] {
+    implicit def corecurseH3[F[_[_], _, _, _], F2[_[_], _, _, _], G[_], Y, Z, E, C <: Effects](implicit ev: PermuteH3[F, F2], L: Lifter[E, C], F: Applicative[F2[G, Y, Z, ?]], NN: NonNested[C]): Lifter.Aux[E, F2[G, Y, Z, ?] |: C, L.Out] = new Lifter[E, F2[G, Y, Z, ?] |: C] {
       type Out = L.Out
 
-      def apply(e: E) = F.pure(L(e))
+      def apply(e: E) = NN.pack[F2[G, Y, Z, ?], Out](F.pure(L(e)))
     }
-  }*/
+  }
 
   @implicitNotFound("could not infer effect stack ${C} from type ${E}")
   trait Wrapper[E, C <: Effects] {
@@ -1006,8 +997,8 @@ final case class Emm[C <: Effects, A](run: C#Point[A]) {
   def flatMap[B](f: A => Emm[C, B])(implicit B: Binder[C]): Emm[C, B] =
     Emm(B.bind(run) { a => f(a).run })
 
-  /*def flatMapM[E](f: A => E)(implicit E: Lifter[E, C], B: Binder[C]): Emm[C, E.Out] =
-    flatMap { a => Emm(E(f(a))) }*/
+  def flatMapM[E](f: A => E)(implicit E: Lifter[E, C], B: Binder[C]): Emm[C, E.Out] =
+    flatMap { a => Emm(E(f(a))) }
 
   def expand(implicit C: Expander[C]): Emm[C.Out, C.CC[A]] = Emm(C(run))
 
